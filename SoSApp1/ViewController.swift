b0f85a2776aa4f4b89903229d71aac451a176084
Souvik <roy369344@gmail.com>
2024-08-02 22:37:29 +0530

    Initial Commit

---
+++ SoSApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoSApp1
+//
+//  Created by M sai deepthi on 02/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
